fb7c7eee3ea4238fc3aeb8948594fc066651fdfd
maijujen <maijujen@gmail.com>
2026-04-13 01:14:53 +0300

    1.1
    Hauling ass has begun

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace AH2721
+{
+
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        }
+    }
+}
+
