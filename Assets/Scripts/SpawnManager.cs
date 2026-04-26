56338697796e0aebe3fba998d764c520dc7a09cb
maijujen <maijujen@gmail.com>
2026-04-26 20:22:05 +0300

    6.0

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace AH2721
+{
+    public class SpawnManager : MonoBehaviour
+    {
+        public GameObject[] animalPrefabs;
+        private float spawnRangeX = 20.0f;
+        private float spawnPosZ = 20.0f;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                int animalIndex = Random.Range(0, animalPrefabs.Length);
+                Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+                Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            }
+        }
+    }
+
+}
